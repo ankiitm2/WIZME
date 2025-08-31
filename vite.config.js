f75d8671d9792f71d3ed5fe1e7c2460e1ab7155b
Ankit Mishra <ankitmishra07am@gmail.com>
2025-08-31 14:54:10 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/",
+  base: "./", // Use relative paths for GitHub Pages
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+  },
 });
