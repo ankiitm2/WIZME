fe787d09013f8c56d60d028d40cd5d334cd4bb41
Ankit Mishra <ankitmishra07am@gmail.com>
2025-09-01 15:44:39 +0530

    routing

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    plugins: [tailwindcss(), react()],
-    base: env.VITE_BASE_PATH || "/",
-    build: {
-      outDir: "dist",
-      assetsDir: "assets",
-      sourcemap: false,
-    },
-  };
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
+  base: process.env.NODE_ENV === "production" ? "/wizme/" : "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+  },
 });
