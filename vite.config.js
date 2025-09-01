b4587d1b1adbbe86abde4851ff250efecd08a9cc
Ankit Mishra <ankitmishra07am@gmail.com>
2025-09-01 15:26:48 +0530

    routing

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "./", // Use relative paths for GitHub Pages
+  base: "/wizme/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
