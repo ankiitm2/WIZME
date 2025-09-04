b3ee42f82075a3e01d1808825bf7736987d988d7
Ankit Mishra <ankitmishra07am@gmail.com>
2025-09-04 09:23:23 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: process.env.NODE_ENV === "production" ? "/wizme/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/WIZME/" : "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
